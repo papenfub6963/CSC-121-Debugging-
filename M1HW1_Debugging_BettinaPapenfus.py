a65dab892bc43d326c855c94defc41f2ef41e485
papenfub6963 <55771514+papenfub6963@users.noreply.github.com>
2020-01-28 07:58:58 -0500

    Add files via upload

---
+++ M1HW1_Debugging_BettinaPapenfus.py
@@ -0,0 +1,29 @@
+# Exercise 3.7 page 112
+# january 23, 2020
+# CSC 121 M1HW1-Debugging
+# Bettina Papenfus
+
+print(" ")
+print(" ")
+print('Number\t Square\t Cube')
+print('------------------------')
+for num in range(0, 6):
+    print(num, '\t', num ** 2, '\t', num ** 3)
+    
+    
+
+##################################################
+# use the for and range statement                #
+# Print break line to seperate                   #
+# use a space statement to add a brea            #                         
+# Create a code that calculates square and cube  #
+# Print the results in table format              #
+##################################################
+    
+    
+print(" ")
+print(" ")
+print('Number\t Square\t Cube')
+print('------------------------')
+for num in range(6, 0, -1):
+    print(num, '\t', num ** 2, '\t', num ** 3)
